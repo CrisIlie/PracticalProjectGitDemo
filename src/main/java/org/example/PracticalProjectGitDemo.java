9a1e30e92ecdded9e5f759836f9c1871992bd2b4
Cristina <crismisio@yahoo.com>
2022-09-25 11:58:39 +0300

    ABC-2 Added project stopped message.

--- src/main/java/org/example/PracticalProjectGitDemo.java
+++ src/main/java/org/example/PracticalProjectGitDemo.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class PracticalProjectGitDemo {
     public static void main(String[] args) {
-
         System.out.println("Project started!");
+        System.out.println("Project stopped.");
     }
 }
